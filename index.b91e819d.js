a1c81187db7e81d1356278de36875639c02bb4e7
Ozozai12 <101360953+Ozozai12@users.noreply.github.com>
2022-09-08 19:42:16 +0000

    Deploying to gh-pages from @ Ozozai12/filmoteka@e13d9a507b222b36c60b6cd5aa10da4247da0a79 🚀

--- index.b91e819d.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".to-top").addEventListener("click",(()=>{window.scrollTo(0,0)}));const o=document.querySelector(".to-top");window.onscroll=()=>{window.scrollY>700?o.classList.remove("to-top--hide"):window.scrollY<700&&o.classList.add("to-top--hide")};
-//# sourceMappingURL=index.b91e819d.js.map
--- index.dbc31efd.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector(".to-top").addEventListener("click",(function(){window.scrollTo(0,0)}));var o=document.querySelector(".to-top");window.onscroll=function(){window.scrollY>700?o.classList.remove("to-top--hide"):window.scrollY<700&&o.classList.add("to-top--hide")}}();
-//# sourceMappingURL=index.dbc31efd.js.map
