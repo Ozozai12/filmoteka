b5c5686046329636fed1ae6e3f048a4c58a2b351
OlgaMykhailova <rosebud_z@yahoo.com>
2022-09-09 16:55:55 +0200

    changed card styles

--- src/js/card.js
+++ src/js/card.js
@@ -31,29 +31,3 @@ function cardMarkup(data) {
     const realeaseData = el.release_date.slice(0, 4);
   });
 }
-
-
-<div class="Gallery">
-      <div class="Card">
-        <div class="CardThumb">
-          <img
-            id=""
-            src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/zHQy4h36WwuCetKS7C3wcT1hkgA.jpg"
-            alt=""
-            loading="lazy"
-            class="CardImage"
-          />
-          <div class="CardOverlay">
-            <p class="CardText">
-              An army ranger and his dog embark on a road trip along the Pacific Coast Highway to attend a friend's funeral.
-            </p>
-          </div>
-        </div>
-        <h2 class="CardTitle">Dog</h2>
-        <div class="CardCaption">
-           <span class="CardInfo">Drama, Fantastics | 2020</span>
-          </div>
-          <span class="CardRating">7,8</span>
-        </div>
-      </div>
-   
