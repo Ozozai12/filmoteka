3ec685cb8fa48fadf278ee1a4cafd1657c3bfb69
Климусь Влад <vladklimus@gmail.com>
2022-09-10 17:18:12 +0300

    примитивная пагинации

---
+++ src/js/pagination.js
@@ -0,0 +1,99 @@
+
+// import NewServiceApi from "./authorization";
+// import text from "./popular"
+// import { NewServiceApi } from './authorization';
+// import { substitutionOfValues } from './card';
+// import createMarkupCard from './templates/cardMarkup.hbs';
+// export {popularMovies}
+
+// const newServiceApi = new NewServiceApi()
+// console.log(newServiceApi.serviceMovieTopApi())   
+// const a = newServiceApi.serviceMovieTopApi().then( res => {
+//    console.log(res);
+//    return res;})
+// // console.log(a)
+// const b = newServiceApi.serviceMovieTopApi()
+
+// const page = newServiceApi.page 
+// console.log(page);
+
+// const nextBtn = document.querySelector('.next')
+// nextBtn.addEventListener("click", onClickNextBtn)
+
+
+
+// document.querySelector('.next').addEventListener('click', e => { 
+//   newServiceApi.incrementPage()
+//  text()
+// })
+
+
+// document.querySelector('.next').addEventListener('click', e => { 
+//   newServiceApi.incrementPage()
+//  text()
+// })
+
+
+// document.querySelector('.next').addEventListener('click',onClickNextBtn)
+  
+
+//   export default  function onClickNextBtn(){
+//   // newServiceApi.serviceMovieTopApi().then(res=> {newServiceApi.incrementPage()
+//   //     console.log(res)})
+//  newServiceApi.incrementPage()
+// //  text()
+
+//  }
+
+//  export default  async function popularMovies() {
+//   const gallery = document.querySelector('.gallery');
+
+//   const popular = await newServiceApi.serviceMovieTopApi();
+//   substitutionOfValues(popular.results);
+//   console.log(popular.results);
+
+//   gallery.innerHTML = createMarkupCard(popular.results);
+// }
+// popularMovies();
+
+
+ 
+
+//  document.querySelector('.next').addEventListener('click', e => { 
+//     newServiceApi.incrementPage()
+//     popularMovies() 
+//   })
+  
+//  async function popularMovies() {
+//   const gallery = document.querySelector('.gallery');
+
+//   const popular = await newServiceApi.serviceMovieTopApi();
+//   substitutionOfValues(popular.results);
+//   console.log(popular.results);
+
+//   gallery.innerHTML = createMarkupCard(popular.results);
+// }
+
+
+
+// popularMovies();
+
+    
+
+
+   
+    
+ 
+  // console.log(s)
+  // b.then(res => res.incrementPage())
+
+
+// function getData(){
+
+// }
+// function onArrowRightClick() {
+//   if (newServiceApi.page < totalPages) {
+//     newServiceApi.pageNumber = newServiceApi.incrementPage();
+//     drawPageWhenClickOnArrow();
+//   }
+// }
