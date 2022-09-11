57ef6b971290f6fc616c8493b8084ad81554a071
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-09-11 23:41:14 +0300

    add button logout +no scroll on open modal
    add button "logout" on modal autorisation +no scroll body on open modal autorisation

--- src/js/header.js
+++ src/js/header.js
@@ -1,17 +1,36 @@
-(() => {
-  const refs = {
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', onOpenModal);
+//   refs.closeModalBtn.addEventListener('click', onCloseModal);
+
+//   function onOpenModal() {
+//     refs.modal.classList.add('is-hidden');
+//     document.body.style.overflow = "hidden";
+//   }
+// })();
+const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
-  };
+}
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.openModalBtn.addEventListener('click', onOpenModal);
+refs.closeModalBtn.addEventListener('click', onCloseModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+function onOpenModal() {
+  refs.modal.classList.remove('is-hidden');
+  document.body.style.overflow = "hidden";
+}
+
+function onCloseModal() {
+  refs.modal.classList.add('is-hidden');
+  document.body.style.overflow = "";
+}
 
 // const loginText = document.querySelector(".title-text .login");
 //          const loginForm = document.querySelector("form.login");
