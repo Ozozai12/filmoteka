cca89ef1037372b2176625c4f26e6b5e16e36676
Климусь Влад <vladklimus@gmail.com>
2022-09-11 21:28:55 +0300

    пагинация стартовая страница

--- src/js/popular.js
+++ src/js/popular.js
@@ -1,103 +1,74 @@
 import { NewServiceApi } from './authorization';
 import { substitutionOfValues } from './card';
 import createMarkupCard from './templates/cardMarkup.hbs';
+import Pagination from 'tui-pagination';
 
 const newserviceApi = new NewServiceApi();
-let totalPages;
-const nextBtn = document.querySelector('.next')
-  nextBtn.addEventListener("click", onClickNextBtn)
-  const prevBtn = document.querySelector('.prev')
-  prevBtn.addEventListener("click", onClickPrevBtn)
+
+const tuiBox = document.getElementById("tui-pagination-container")
+
+tuiBox.addEventListener('click', testClick)
+function testClick(e){
+
   
+  const pageList = Number(e.target.textContent)
+  // console.log(e.target.textContent);
+  if(pageList > 0){
+    window.scrollTo(0, 0)
+  newserviceApi.pageNumber = pageList
+  popularMovies();
+    
+  } 
   
+}
+
+
+
 async function popularMovies() {
   const gallery = document.querySelector('.gallery');
 
   const popular = await newserviceApi.serviceMovieTopApi().then( res => {
-      console.log(res);
+
+    const container = document.getElementById('tui-pagination-container');
+    const options = { 
+         totalItems: res.total_results,
+         itemsPerPage: 20,
+        
+         page: res.page ,
+         centerAlign: true,
+         firstItemClassName: 'tui-first-child',
+         lastItemClassName: 'tui-last-child',
+         template: {
+             page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+             currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+             moveButton:
+                 '<a href="#" class="tui-page-btn tui-{{type}}">' +
+                     '<span class="tui-ico-{{type}}">{{type}}</span>' +
+                 '</a>',
+             disabledMoveButton:
+                 '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+                     '<span class="tui-ico-{{type}}">{{type}}</span>' +
+                 '</span>',
+             moreButton:
+                 '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+                     '<span class="tui-ico-ellip">...</span>' +
+                 '</a>'
+         }
+    };
+
+    const pagination = new Pagination(container, options);
+    
+
+      // console.log(res);
       totalPages = res.total_results
-      console.log(totalPages);
+      // console.log(totalPages);
       return res;});
   substitutionOfValues(popular.results);
 
-
-  gallery.innerHTML = createMarkupCard(popular.results);
-
-  displayPagination(totalPages);
- 
-
   
+gallery.innerHTML = createMarkupCard(popular.results);
 }
-function test (){
 
-   if(newserviceApi.page === 1){
-    prevBtn.classList.add("is__hidden")
-  }
-  if(newserviceApi.page > 1){
-    prevBtn.classList.remove("is__hidden")
-  }
-}
+
 
 popularMovies();
-
-
-function onClickNextBtn(){
-    // newServiceApi.serviceMovieTopApi().then(res=> {newServiceApi.incrementPage()
-    //     console.log(res)})
-   newserviceApi.incrementPage()
-   popularMovies()
-   window.scrollTo(0, 0);
-  //  const a = newserviceApi.serviceMovieTopApi().then( res => {
-  //   console.log(res);
-  //   return res;})
-   
-}
-
-   function onClickPrevBtn(){
-    
-    newserviceApi.decrementPage()
-   popularMovies()
-   window.scrollTo(0, 0);
-   }
-
- 
-  function displayPagination(totalPages) {
-    const paginationEl = document.querySelector('.pagination');
-    const pagesCount = Math.ceil(totalPages / 20);
-    const ulEl = document.createElement("ul");
-    ulEl.classList.add('pagination__list');
-
-    for (let i = 0; i < pagesCount; i++) {
-      const liEl = displayPaginationBtn(i + 1);
-      ulEl.appendChild(liEl)
-    }
-    paginationEl.appendChild(ulEl)
-    test()
-  }
-  function displayPaginationBtn(page) {
-    const liEl = document.createElement("li");
-    liEl.classList.add('pagination__item')
-    liEl.innerText = page
-
-    // if (pageNumber == page) liEl.classList.add('pagination__item--active');
-
-    liEl.addEventListener('click', (e) => {
-      const page = e.currentTarget.textContent
-      newserviceApi.pageNumber = Number(page);
-      // console.log(pageNumber);
-      console.log(page);
-      popularMovies();
-
-      // let currentItemLi = document.querySelector('li.pagination__item--active');
-      // currentItemLi.classList.remove('pagination__item--active');
-
-      // liEl.classList.add('pagination__item--active');
-    })
-
-    return liEl;
-
-
-
-
-   
-  }
