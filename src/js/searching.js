b29dcb2d08378802573fbe3f575e22fbd11159e6
Artem Karpenko <dante120591@gmail.com>
2022-09-12 11:48:27 +0300

    Fixed conflicts

--- src/js/searching.js
+++ src/js/searching.js
@@ -70,20 +70,20 @@ function createSearchingList(event) {
   }
 
 
-    if (event.target.value === '') {
-        searchingList.innerHTML = '';
-        return;        
-    }
+  if (event.target.value === '') {
+    searchingList.innerHTML = '';
+    return;
+  }
 
-    newServiceApi.searchValue = event.target.value;
+  newServiceApi.searchValue = event.target.value;
 
-    newServiceApi.serviceSearchMovie().then(res => {
+  newServiceApi.serviceSearchMovie().then(res => {
       
 
 
-  newServiceApi.serviceSearchMovie().then(res => {
-    if (res.results.length === 0) {
-      const errorNotificationMarkup = `
+    newServiceApi.serviceSearchMovie().then(res => {
+      if (res.results.length === 0) {
+        const errorNotificationMarkup = `
             <li>
                 <p class="error-notification">
                     Search result not successful. Enter the correct movie name and try
@@ -91,15 +91,15 @@ function createSearchingList(event) {
                 </p>
             </li>`;
 
-      searchingList.innerHTML = errorNotificationMarkup;
-      return;
-    }
+        searchingList.innerHTML = errorNotificationMarkup;
+        return;
+      }
 
-    const searchingListMarkup = res.results
-      .slice(0, 5)
-      .map(movie =>
-        movie.poster_path
-          ? `<li class="searching-list__item" id='${movie.id}'>
+      const searchingListMarkup = res.results
+        .slice(0, 5)
+        .map(movie =>
+          movie.poster_path
+            ? `<li class="searching-list__item" id='${movie.id}'>
             <img
               class="searching-list__photo"
               src="https://image.tmdb.org/t/p/original/${movie.poster_path}"
@@ -108,7 +108,7 @@ function createSearchingList(event) {
             />
             <p class="searching-list__title">${movie.title}</p>
         </li>`
-          : `<li class="searching-list__item" id='${movie.id}'>
+            : `<li class="searching-list__item" id='${movie.id}'>
             <img
               class="searching-list__photo"
               src="https://cdn.pixabay.com/photo/2014/03/25/16/27/movie-297135_960_720.png"
@@ -123,64 +123,62 @@ function createSearchingList(event) {
             
 
 
-    searchingList.innerHTML = searchingListMarkup;
+      searchingList.innerHTML = searchingListMarkup;
 
-    searchingList.addEventListener('click', onMovieClick);
-  });
+      searchingList.addEventListener('click', onMovieClick);
+    });
+  })
 }
 
 function createMoviesList(event) {
 
-    event.preventDefault();
-
-    newServiceApi.searchValue = event.target.search.value;
-
-    // Пагинация 
-    const delBox = document.getElementById("tui-pagination-container")
-    delBox.innerHTML = ''
-    // Пагинация
-
-    newServiceApi.serviceSearchMovie().then(res => {
-        console.log(res);
-
-
-                        // Пагинация
-                        const container = document.getElementById('tui-pagination-container__search');
-                        const options = { 
-                            totalItems: res.total_results,
-                            itemsPerPage: 20,
-                            
-                            page: res.page ,
-                            centerAlign: true,
-                            firstItemClassName: 'tui-first-child',
-                            lastItemClassName: 'tui-last-child',
-                            template: {
-                                page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-                                currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-                                moveButton:
-                                    '<a href="#" class="tui-page-btn tui-{{type}}">' +
-                                        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-                                    '</a>',
-                                disabledMoveButton:
-                                    '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-                                        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-                                    '</span>',
-                                moreButton:
-                                    '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-                                        '<span class="tui-ico-ellip">...</span>' +
-                                    '</a>'
-                            }
-                        };                  
-                        const pagination = new Pagination(container, options);
-                    
-                        // Пагинация
-
+  event.preventDefault();
 
   newServiceApi.searchValue = event.target.search.value;
 
+  // Пагинация 
+  const delBox = document.getElementById("tui-pagination-container")
+  delBox.innerHTML = ''
+  // Пагинация
+
   newServiceApi.serviceSearchMovie().then(res => {
-    if (res.results.length === 0) {
-      const errorNotificationMarkup = `
+    console.log(res);
+
+
+    // Пагинация
+    const container = document.getElementById('tui-pagination-container__search');
+    const options = {
+      totalItems: res.total_results,
+      itemsPerPage: 20,
+                            
+      page: res.page,
+      centerAlign: true,
+      firstItemClassName: 'tui-first-child',
+      lastItemClassName: 'tui-last-child',
+      template: {
+        page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+        currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+        moveButton:
+          '<a href="#" class="tui-page-btn tui-{{type}}">' +
+          '<span class="tui-ico-{{type}}">{{type}}</span>' +
+          '</a>',
+        disabledMoveButton:
+          '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+          '<span class="tui-ico-{{type}}">{{type}}</span>' +
+          '</span>',
+        moreButton:
+          '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+          '<span class="tui-ico-ellip">...</span>' +
+          '</a>'
+      }
+    };
+    const pagination = new Pagination(container, options);
+                    
+    // Пагинация
+
+    newServiceApi.serviceSearchMovie().then(res => {
+      if (res.results.length === 0) {
+        const errorNotificationMarkup = `
             <li>
                 <p class="error-notification">
                     Search result not successful. Enter the correct movie name and try
@@ -189,28 +187,23 @@ function createMoviesList(event) {
             </li>`;
 
 
-            searchingList.innerHTML = errorNotificationMarkup;    
-            return;
-        }
-        searchingList.innerHTML = '';
+        searchingList.innerHTML = errorNotificationMarkup;
+        return;
+      }
+      searchingList.innerHTML = '';
 
                 
 
-        substitutionOfValues(res.results);
+      substitutionOfValues(res.results);
 
-        gallery.innerHTML = createCardMarkup(res.results)
+      gallery.innerHTML = createCardMarkup(res.results)
     })
 
-};
-
-
-    substitutionOfValues(res.results);
-
-    gallery.innerHTML = createCardMarkup(res.results);
-  });
+  })
 }
 
 if (gallery) {
-  form.addEventListener('submit', createMoviesList);
-  input.addEventListener('input', createSearchingList);
-}
+    form.addEventListener('submit', createMoviesList);
+    input.addEventListener('input', createSearchingList);
+  }
+
