ae2612b87782da9d8d98c7c689d590d06929ff2f
oreshnyk <55344626+oreshnyk@users.noreply.github.com>
2022-09-12 17:49:50 +0200

    changed size of button to top

--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -1,1 +1,16 @@
 import { modalMarkUp } from './modal';
+
+const btnClose = document.querySelector('.button__modal-close');
+btnClose.addEventListener('click', onModalClose);
+
+function onModalClose() {
+    modalBox.innerHTML = '';
+}
+
+function onModalCloseEsc(evt) {
+    if (evt.code === 'Escape' &&  modalBox.innerHTML === '') {     
+        return
+    } else if (evt.code === 'Escape') {
+        onModalClose();
+    }
+}
